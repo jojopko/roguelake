3e7066ae65882694a95d8b8c334769ae20b98c09
jojopko <gamerat.github@gmail.com>
2023-03-06 22:17:59 +0300

    add my work

--- sources/level.h
+++ sources/level.h
@@ -1,3 +1,32 @@
+#ifndef _LEVEL_
+#define _LEVEL_
+
 // Карту
 // Камеру
 // перемещение камеры
+
+struct Camera {
+    int offset_x;
+    int offset_y;
+    int w;
+    int h;
+    char * surface;
+};
+
+struct Level {
+    int w;
+    int h;
+    char * field;
+};
+
+Camera * init_camera();
+
+Level * init_level();
+
+bool load_level(const char * filename, Level * level);
+
+void draw_level(Camera * camera, const Level * level);
+
+void draw(Camera * camera);
+
+#endif
