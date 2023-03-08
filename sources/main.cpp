a1341938750b61ad1587c7dae1c29cc06b5cb977
jojopko <gamerat.github@gmail.com>
2023-03-08 10:42:02 +0000

    add test move code

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,29 @@
-
+#include <stdlib.h>
+#include "figth.h"
+#include "level.h"
 
 int main() {
 
+
     return 0;
 }
+
+
+// Тестовый код для движения. Закинуть в main и раскоментить
+    // Player * player = Player_Init(9,9,100,10);
+    // Enemy * enemy = Enemy_Init(1,0,10,100);
+
+    // Camera * cam = init_camera();
+    // Level * level = init_level();
+    // if (!load_level("../assets/level1.txt\0", level))
+    //     return 2;
+
+    // while (true){
+    //     draw_level(cam, level);
+    //     // draw_item(cam, )
+    //     draw_player(cam, player);
+    //     draw(cam);
+    //     movePlayer(player, level);
+    //     clear(cam);
+    // }
 
